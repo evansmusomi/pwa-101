9681fc60f72762f28689fceb6b5ff9a56a2ffded
Evans Musomi <evans.musomi@gmail.com>
2018-04-10 07:27:21 +0300

    prettifies code

--- service-worker/main.js
+++ service-worker/main.js
@@ -1,8 +1,11 @@
 // Progressive Enhancement (SW support)
 
-if (navigator.serviceWorker){
-    // register the SW
-    navigator.serviceWorker.register('/sw.js').then(function(registration){
-        console.log('SW Registered');
-    }).catch(console.log());
+if (navigator.serviceWorker) {
+  // register the SW
+  navigator.serviceWorker
+    .register("./sw.js")
+    .then(function(registration) {
+      console.log("SW Registered");
+    })
+    .catch(console.log());
 }
--- service-worker/sw.js
+++ service-worker/sw.js
@@ -1,13 +1,13 @@
 //SW
 
-self.addEventListener('install', (e) => {
-    console.log('SW: Install Event');
+self.addEventListener("install", e => {
+  console.log("SW: Install Event");
 });
 
-self.addEventListener('activate', (e) => {
-    console.log('SW: Activate Event');
+self.addEventListener("activate", e => {
+  console.log("SW: Activate Event");
 });
 
-self.addEventListener('fetch', (e) => {
-    console.log('Fetch Event: ' + e.request.url); 
+self.addEventListener("fetch", e => {
+  console.log("Fetch Event: " + e.request.url);
 });
