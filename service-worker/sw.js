8ce0fb1337c973296e738ebfdb9f6c47ecfd13e3
Evans Musomi <evans.musomi@gmail.com>
2018-04-10 02:30:26 +0000

    adds network request detection to sw

--- service-worker/sw.js
+++ service-worker/sw.js
@@ -7,3 +7,7 @@ self.addEventListener('install', (e) => {
 self.addEventListener('activate', (e) => {
     console.log('SW: Activate Event');
 });
+
+self.addEventListener('fetch', (e) => {
+    console.log('Fetch Event: ' + e.request.url); 
+});
